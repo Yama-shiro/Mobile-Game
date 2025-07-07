95a432d0e457fa9443a6cd2b8520e5d50abfb790
Yama-shiro <nicolasgyamashiro@gmail.com>
2025-07-07 09:31:36 -0300

    mudanças

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,16 +4,43 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-
+    //publics
     [Header("Lerp")]
     public Transform target;
     public float lerpSpeed = 1f;
 
     public float speed = 1f;
 
+    public string tagToCheckEnemy = "Enemy";
+
+    //privates
+    private bool _canRun;
+    private Vector3 _pos;
+
+
+    private void start()
+    {
+        _canRun = true;
+    }
+
     void Update()
     {
+        if(!_canRun) return;
+
+
+        var pos = target.position;
+        pos.y = transform.position.y;
+        pos.z = transform.position.z;
+
         transform.position = Vector3.Lerp(transform.position, target.position, lerpSpeed * Time.deltaTime);
         transform.Translate(transform.forward * speed *Time.deltaTime);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.tag == tagToCheckEnemy)
+        {
+            _canRun = false;
+        }
     }
 }
