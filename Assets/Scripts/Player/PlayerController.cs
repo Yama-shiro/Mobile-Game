d483f70d9b9abd8d63d0226d093bc375b0446d15
Yama-shiro <nicolasgyamashiro@gmail.com>
2025-07-11 07:44:40 -0300

    powerups

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,8 +1,9 @@
+using Ebac.Core.Singleton;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController : Singleton<PlayerController>
 {
     //publics
     [Header("Lerp")]
@@ -17,8 +18,17 @@ public class PlayerController : MonoBehaviour
     //privates
     private bool _canRun;
     private Vector3 _pos;
+    private float _currentSpeed;
+    private Vector3 _startPosition;
 
     public GameObject endScreen;
+    public bool invencible = false;
+
+    private void Start()
+    {
+        _startPosition = transform.position;
+        ResetSpeed();
+    }
 
     void Update()
     {
@@ -30,22 +40,23 @@ public class PlayerController : MonoBehaviour
         pos.z = transform.position.z;
 
         transform.position = Vector3.Lerp(transform.position, target.position, lerpSpeed * Time.deltaTime);
-        transform.Translate(transform.forward * speed *Time.deltaTime);
+        transform.Translate(transform.forward * _currentSpeed * Time.deltaTime);
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.transform.tag == tagToCheckEnemy)
+        if (collision.transform.tag == tagToCheckEndLine)
         {
-            EndGame();
+            if (!invencible) EndGame();
         }
+
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.transform.tag == tagToCheckEndLine)
+        if (other.transform.tag == tagToCheckEndLine)
         {
-            EndGame();
+            if (!invencible) EndGame();
         }
     }
 
@@ -59,4 +70,26 @@ public class PlayerController : MonoBehaviour
     {
         _canRun = true;
     }
+
+    #region PowerUps
+
+    public void SetPowerUpText(string s)
+    {
+        //uiTextPowerUp.text = s;
+    }
+    public void PowerUpSpeedUp(float f)
+    {
+        _currentSpeed = f;
+    }
+    public void ResetSpeed()
+    {
+        _currentSpeed = speed;
+    }
+
+    public void SetInvencible(bool b = true)
+    {
+        invencible = b;
+    }
+
+    #endregion
 }
